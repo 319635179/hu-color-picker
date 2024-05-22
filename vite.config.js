5dcc19df3dbee4aa70b3d3e5111a6158ee10351e
wangzh <57377498+319635179@users.noreply.github.com>
2024-05-22 19:14:01 +0800

    object run

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 7000,
+    open: true
+  },
+  optimizeDeps: {
+    include: [
+      'vue',
+    ]
+  }
 })
